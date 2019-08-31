a03fc318ea37d253fd6edfc89d8fbb8e5c34de3b
fudaiyue <fudaiyue@zhiketong.cn>
2019-09-01 00:35:19 +0800

    zh support

---
+++ r18/koa/router/ranking.js
@@ -0,0 +1,34 @@
+const { getRecentClicksFormatted } = require('../../sequelize/methods/recentClick.js');
+
+
+module.exports = async (ctx, next) => {
+	let days = parseInt(ctx.query.days || '') || 1;
+
+	if (![1, 7, 30].includes(days)) days = 7;
+	let recentClicksFormatted = await getRecentClicksFormatted({
+		days
+	});
+
+	let mapping = [{
+		en: 'Top Videos',
+		zh: '热门影片'
+	},{
+		en: 'Top Casts',
+		zh: '热门女优'
+	},{
+		en: 'Top Genre',
+		zh: '热门分类'
+	},{
+		en: 'Top Studios',
+		zh: '热门发行商'
+	}]
+	ctx.body = ctx.dots.index({
+		type: 'ranking',
+		pageTitle: 'Ranking',
+		columnMap: mapping,
+		ranking: recentClicksFormatted,
+		days
+	});
+
+	return;
+}
