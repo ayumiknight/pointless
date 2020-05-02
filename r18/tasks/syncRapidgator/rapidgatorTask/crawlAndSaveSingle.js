08f4913c507ef2d2a0bc7e3342ae03d81d422b3e
fudaiyue <fudaiyue.zhiketong.cn>
2020-05-02 21:39:33 +0800

    batch rapidgator for speed

--- r18/tasks/syncRapidgator/rapidgatorTask/crawlAndSaveSingle.js
+++ r18/tasks/syncRapidgator/rapidgatorTask/crawlAndSaveSingle.js
@@ -1,15 +1,14 @@
 const tryGetRapidgatorLinkJavArchive = require('./tryGetRapidgatorLinkJavArchive.js');
 //const tryGetRapidgatorLinkFansJav = require('./tryGetRapidgatorLinkFansJav.js');
-const Rapidgator = require('./rapidgator.js');
+
 
 async function crawlAndSaveSingle({
-	code
+	code,
+	R
 }) {
 	let javInfo = await tryGetRapidgatorLinkJavArchive({
 		code
 	});
-	let R = new Rapidgator();
-	await R.login();
 	let myLinks = await R.saveLinksToFolder({
 		name: code,
 		fileLinks: javInfo.rapidgator
--- r18/tasks/syncRapidgator/syncRapidgator.js
+++ r18/tasks/syncRapidgator/syncRapidgator.js
@@ -3,6 +3,7 @@
 const { getR18WithExtraPaged } = require('../../sequelize/methods/index.js');
 const { sequelize, Sequelize, Extra } = require('../../sequelize/index.js');
 const crawlAndSaveSingle = require('./rapidgatorTask/crawlAndSaveSingle.js');
+const Rapidgator = require('./rapidgatorTask/rapidgator.js');
 
 let rapidgatorPageAllFromArgv = process.argv.find(one => one.match(/^--rapidgatorPage(\d+)$/))
 let rapidgatorPageAll = rapidgatorPageAllFromArgv ? rapidgatorPageAllFromArgv.replace(/^--rapidgatorPage(\d+)$/i, '$1') : 5;
@@ -13,39 +14,48 @@ async function syncRapidgator(all) {
 	let page = 1;
 	let pageNum = rapidgatorPageAll;
 
+	let R = new Rapidgator();
+	await R.login();
+
 	while (page <= pageNum) {
 		let rows = await getR18WithExtraPaged({
 			page,
 			pagesize: 20
 		});
 		rows = rows || [];
-		let index = 0;
-		while(index <= rows.length - 1 ) {
-			let row = rows[index],
-				{ id, Extras, code } = rows[index] || {};
 
-			if (row && id && !Extras) {
-				try {
-					let extras = await crawlAndSaveSingle({
-						code: code.replace('-', ' ')
-					});
-					console.log(extras, `=================extras retrieved ${code}  ${id}\n`)
-					let saveExtras = await Extra.findOrCreate({
-						where: {
-							R18Id: id
-						},
-						defaults: {
-							extra: JSON.stringify(extras)
-						}
-					});
-					console.log(`${code} ${id} ${saveExtras} crawled and saved\n`);
-				} catch(e) {
-					console.log(`!!!!!!!!!!!!! ${code} ${id}==${e.message}\n`);
-				}	
-			}
-			console.log(`!!!!!!!!!!!!! page ${index} rapidgator complete`)
-			index++;
-		}
+		await Promise.all(rows.filter( row => {
+			let {
+				id,
+				Extras
+			} = row;
+			return row && id && !Extras;
+		}).map(async row => {
+			let {
+				id,
+				code
+			} = row;
+
+			try {
+				let extras = await crawlAndSaveSingle({
+					code: code.replace('-', ' '),
+					R
+				});
+				console.log(extras, `=================extras retrieved ${code}  ${id}\n`)
+				let saveExtras = await Extra.findOrCreate({
+					where: {
+						R18Id: id
+					},
+					defaults: {
+						extra: JSON.stringify(extras)
+					}
+				});
+				console.log(`${code} ${id} ${saveExtras} crawled and saved\n`);
+			} catch(e) {
+				console.log(`!!!!!!!!!!!!! ${code} ${id}==${e.message}\n`);
+			}	
+		}))
+		console.log(`!!!!!!!!!!!!! page ${page} rapidgator complete`)
 		page++;
 	}
 }
