0d5b95a9091d3ce92d538153c204aaaaf7fc80e8
fudaiyue <fudaiyue.zhiketong.cn>
2020-06-05 21:52:18 +0800

    fix bug

--- r18/puppeteerAutoTask/javlibraryAutoPost.js
+++ r18/puppeteerAutoTask/javlibraryAutoPost.js
@@ -55,6 +55,7 @@ class JavlibraryAutoPost {
 		this.page = await this.browser.newPage();
 		this.page.setDefaultNavigationTimeout(5 * 60 * 1000);
 		await this.page.setUserAgent(userAgent);
+		await this.login();
 		
 		fs.readdirSync('./captcha').map(f => {
 			let [name, ext] = f.split('.'),
