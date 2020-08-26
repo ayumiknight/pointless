9a023c2c8602ee0ca8aaa6722ee85fba718098bf
664826945@qq.com <664826945@qq.com>
2020-08-26 22:05:35 +0800

    fix bug

--- r18/koa/imageDownload.js
+++ r18/koa/imageDownload.js
@@ -5,8 +5,9 @@ const fse = require('fs-extra');
 const send = require('koa-send');
 
 module.exports = async function(ctx, next) {
-  if (ctx.path.match(/^\/static/i) && ctx.status === 404 && ctx.path.match(/^.+\.(jpg|png|jpeg)$/) && ctx.method === 'GET') {
+  if (ctx.path.match(/^\/static/i) && ctx.status === 404 && ctx.path.match(/^.+\.(jpg|png|jpeg)$/) && (ctx.method === 'GET' || ctx.method === 'HEAD')) {
     const remotePath = ctx.path.slice(7)
+    console.log(remotePath, '=========remotePath==========')
     const res = await axios.get('https://pics.r18.com' + remotePath, {
       responseType: 'arraybuffer'
     })
