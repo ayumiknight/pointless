ff0e95f6e08b1768f60e0cb5a73f9d1d89ef57f5
664826945@qq.com <664826945@qq.com>
2020-08-26 18:37:30 +0800

    image proxy fix

--- r18/koa/imageDownload.js
+++ r18/koa/imageDownload.js
@@ -6,7 +6,7 @@ const fse = require('fs-extra');
 module.exports = async function(ctx, next) {
   if (ctx.path.match(/^\/static/i) && ctx.status === 404 && ctx.path.match(/^.+\.(jpg|png|jpeg)$/)) {
     const remotePath = ctx.path.slice(7)
-    const res = await axios.get('https://imuse-1300199822.cos.ap-beijing.myqcloud.com' +remotePath)
+    const res = await axios.get('https://pics.r18.com' +remotePath)
     if (res && res.data) {
       await fse.outputFileSync(__dirname + `/static${remotePath}`, res.data)
       ctx.body = res.data
