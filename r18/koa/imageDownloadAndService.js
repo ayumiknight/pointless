5d6c37e561a64e22ef5aa1fb5dcd3b07bfa23b4d
664826945@qq.com <664826945@qq.com>
2020-10-09 20:22:40 +0800

    remove debug

--- r18/koa/imageDownloadAndService.js
+++ r18/koa/imageDownloadAndService.js
@@ -5,7 +5,6 @@ const fse = require('fs-extra');
 const send = require('koa-send');
 
 module.exports = async function(ctx, next) {
-  console.log('ctx path===============', ctx.status, ctx.method, ctx.path, '=========', ctx.path.match(/^.+\.(jpg|png|jpeg|gif)$/))
   if (ctx.path.match(/^\/static/i) && ctx.status === 404 && ctx.path.match(/^.+\.(jpg|png|jpeg|gif)$/) && (ctx.method === 'GET' || ctx.method === 'HEAD')) {
     const remotePath = ctx.path.slice(7)
     // https://imuse-1300199822.cos.ap-beijing.myqcloud.com/img/70e64d5b609f0831c22658aef771b854.jpeg
@@ -15,7 +14,6 @@ module.exports = async function(ctx, next) {
     if (res && res.data) {
       await fse.outputFileSync(__dirname + `/static${remotePath}`, res.data)
       try {
-        console.log('sending from disk after download==============')
         const done = await send(ctx, `/koa/static${remotePath}`, {
           maxAge: 1000 * 60 * 60 * 1
         })
