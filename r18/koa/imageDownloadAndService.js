08e51d8eb3589592e47ab5bdbec62be99a9e6187
664826945@qq.com <664826945@qq.com>
2020-10-09 19:59:32 +0800

    add debug

--- r18/koa/imageDownloadAndService.js
+++ r18/koa/imageDownloadAndService.js
@@ -5,7 +5,7 @@ const fse = require('fs-extra');
 const send = require('koa-send');
 
 module.exports = async function(ctx, next) {
-  console.log('ctx path===============', ctx.path, ctx.path === '/service.js' && ctx.method === 'GET')
+  console.log('ctx path===============', ctx.status, ctx.method, ctx.path, '=========', ctx.path.match(/^.+\.(jpg|png|jpeg|gif)$/))
   if (ctx.path.match(/^\/static/i) && ctx.status === 404 && ctx.path.match(/^.+\.(jpg|png|jpeg|gif)$/) && (ctx.method === 'GET' || ctx.method === 'HEAD')) {
     const remotePath = ctx.path.slice(7)
     // https://imuse-1300199822.cos.ap-beijing.myqcloud.com/img/70e64d5b609f0831c22658aef771b854.jpeg
