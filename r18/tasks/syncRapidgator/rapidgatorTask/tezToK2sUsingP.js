186ceeaba90c84528ae5ebda00b72d9ebd7908b1
fudaiyue <daiyue.fu@faw-vw.com>
2020-11-03 15:58:52 +0800

    md5 identical confirmed with puppeteer, no need to generate

--- r18/tasks/syncRapidgator/rapidgatorTask/tezToK2sUsingP.js
+++ r18/tasks/syncRapidgator/rapidgatorTask/tezToK2sUsingP.js
@@ -64,54 +64,9 @@ async function tezToK2sRp({
     } catch(e) {
       console.log(e.message, e.url, e.response && e.response.data, '===========tez to k2s using p single====', link)
       if (e.message.match('Request failed with status code 406')) {
-        try {
-          const tempUrl = await axios({
-            url: 'https://tezfiles.com/api/v2/getUrl',
-            method: 'POST',
-            data: JSON.stringify({
-              access_token: tezP,
-              file_id: link.replace(/^https\:\/\/tezfiles.com\/file\/([a-z0-9]+)\/.+$/, "$1")
-            })
-          });
-    
-          const headRes = await axios({
-            method: 'HEAD',
-            url: tempUrl.data.url
-          });
-          const extension = link.split('.').pop()
-          const detail = {
-            extension,
-            tempUrl: tempUrl.data.url,
-            md5: headRes.headers.etag.replace(/"/g, ''),
-            contentLength: headRes.headers['content-length'],
-            newName: link.split('/').pop().replace('avcens.xyz','jvrlibrary').replace('avcens', 'jvrlibrary')
-          }
-          console.log(detail, '========tez to k2s fill with tez file detail============')
-          const k2sSaveResult = await axios({
-            url: 'https://keep2share.cc/api/v2/createFileByHash',
-            method: 'POST',
-            data: JSON.stringify({
-              access_token: tezP,
-              hash: detail.md5,
-              name: detail.newName,
-              // parent: k2sTargetFolderId,
-              access: "premium"
-            })
-          })
-          // console.log(k2sSaveResult.data, '======================k2s saveresult==========')
-          // const rpLink = await R.tezToRpSingle({
-          //   newName: detail.newName,
-          //   detail,
-          //   folderId: rpTargetFolderId
-          // })
-          myK2ss.push(k2sSaveResult.data.link + '/' + detail.newName);
-          myK2sIds.push(k2sSaveResult.data.id);
-        } catch(e) {
-          console.log(e.message, e.url, e.response && e.response.data, '===========tez to k2s fill with tez failed====', link)
-          noData406 = true;
-          break;
-        }
-      }  
+        noData406 = true;
+        break;
+      }
     }
     index++;
   }
