8c7f5ecd5ec29de1c771645c784c125344d69273
fudaiyue <daiyue.fu@faw-vw.com>
2020-11-04 18:56:22 +0800

    test

--- r18/tasks/syncRapidgator/rapidgatorTask/test.js
+++ r18/tasks/syncRapidgator/rapidgatorTask/test.js
@@ -2,6 +2,7 @@
 const axios = require('axios');
 const link = 'https://tezfiles.com/file/681b866ee9199/jpsvr00022.part2.VR.avcens.xyz.mp4?utm_campaign=video_online';
 const { _66, tezP, k2sVR, k2sNormal } = require('./k2sConfig');
+const fs = require('fs');
 
 axios({
   url: 'https://tezfiles.com/api/v2/getUrl',
@@ -11,5 +12,8 @@ axios({
     file_id: link.replace(/^https\:\/\/tezfiles.com\/file\/([a-z0-9]+)\/.+$/, "$1")
   })
 }).then(res => {
-  console.log(res.response)
+  console.log(res.data)
+  axios.get(res.data.url).then(response => {
+    response.data.pipe(fs.createWriteStream("a.mp4"));  
+  })
 })
