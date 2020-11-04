4a770ddd44fd8ba92888d9bbf457e83cd3d009ff
fudaiyue <daiyue.fu@faw-vw.com>
2020-11-04 18:49:10 +0800

    add test

---
+++ r18/tasks/syncRapidgator/rapidgatorTask/test.js
@@ -0,0 +1,15 @@
+
+const axios = require('axios');
+const link = 'https://tezfiles.com/file/681b866ee9199/jpsvr00022.part2.VR.avcens.xyz.mp4?utm_campaign=video_online';
+const { _66, tezP, k2sVR, k2sNormal } = require('./k2sConfig');
+
+axios({
+  url: 'https://tezfiles.com/api/v2/getUrl',
+  method: 'POST',
+  data: JSON.stringify({
+    access_token: tezP,
+    file_id: link.replace(/^https\:\/\/tezfiles.com\/file\/([a-z0-9]+)\/.+$/, "$1")
+  })
+}).then(res => {
+  console.log(res.response)
+})
