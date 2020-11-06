2621ac881e3eb950959b1ac08f5e19e2cac1a693
fudaiyue <daiyue.fu@faw-vw.com>
2020-11-06 10:05:57 +0800

    shut down avcens crawl

--- r18/tasks/syncRapidgator/rapidgatorTask/crawlAndSaveSingle.js
+++ r18/tasks/syncRapidgator/rapidgatorTask/crawlAndSaveSingle.js
@@ -41,7 +41,7 @@ async function crawlAndSaveSingle({
             })
         }  
     } catch(e) {
-        if (needK2s && !javInfo1 && vr) {
+        if (false && needK2s && !javInfo1 && vr) {
             javInfo2 = await tryGetTezLinkAvcens({
                 code
             })
